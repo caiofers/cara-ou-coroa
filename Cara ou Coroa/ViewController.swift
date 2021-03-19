d31ac356c38871b9cfc7ab407e1106d9d52f7d38
Caio <caiovr13@gmail.com>
2021-03-18 22:12:21 -0300

    Initial Commit

---
+++ Cara ou Coroa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cara ou Coroa
+//
+//  Created by Caio Fernandes on 18/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
